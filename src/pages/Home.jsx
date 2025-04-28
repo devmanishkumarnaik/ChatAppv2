c466c5615bd3f182ca12bff33de10c1bdad51965
Manish Kumar Naik <165175167+devmanishkumarnaik@users.noreply.github.com>
2025-04-28 07:28:24 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,3 @@
-// import React from 'react'
-// import AppLayout from '../components/layout/AppLayout'
-// import { Box, Typography } from '@mui/material';
-// import { grayColor } from '../constants/color';
-
-// const Home = () => {
-//   return (
-//     <Box bgcolor={grayColor} height={"100%"}>
-//       <Typography p={"2rem"} variant='h5' textAlign={"center"}>
-//           Select a friend to chat
-//       </Typography>
-//     </Box>
-//   )
-// }
-
-// export default AppLayout()(Home);
-
-
-
-
 import React from 'react'
 import AppLayout from '../components/layout/AppLayout'
 import { Box, Typography } from '@mui/material';
@@ -25,30 +5,50 @@ import { grayColor } from '../constants/color';
 
 const Home = () => {
   return (
-    <Box 
-      bgcolor={grayColor} 
-      height={"100%"}
-      display="flex"
-      flexDirection="column"
-    >
-      {/* <Typography p={"2rem"} variant='h5' textAlign={"center"}>
+    <Box bgcolor={grayColor} height={"100%"}>
+      <Typography p={"2rem"} variant='h5' textAlign={"center"}>
           Select a friend to chat
-      </Typography> */}
-      
-      <Box flex={1} position="relative">
-        <iframe
-          src="https://manishinc-chat.onrender.com/" // Replace with your desired website URL
-          style={{
-            width: '100%',
-            height: '100%',
-            border: 'none',
-            backgroundColor: 'white'
-          }}
-          title="Website Preview"
-        />
-      </Box>
+      </Typography>
     </Box>
   )
 }
 
 export default AppLayout()(Home);
+
+
+
+
+// import React from 'react'
+// import AppLayout from '../components/layout/AppLayout'
+// import { Box, Typography } from '@mui/material';
+// import { grayColor } from '../constants/color';
+
+// const Home = () => {
+//   return (
+//     <Box 
+//       bgcolor={grayColor} 
+//       height={"100%"}
+//       display="flex"
+//       flexDirection="column"
+//     >
+//       {/* <Typography p={"2rem"} variant='h5' textAlign={"center"}>
+//           Select a friend to chat
+//       </Typography> */}
+      
+//       <Box flex={1} position="relative">
+//         <iframe
+//           src="https://manishinc-chat.onrender.com/" // Replace with your desired website URL
+//           style={{
+//             width: '100%',
+//             height: '100%',
+//             border: 'none',
+//             backgroundColor: 'white'
+//           }}
+//           title="Website Preview"
+//         />
+//       </Box>
+//     </Box>
+//   )
+// }
+
+// export default AppLayout()(Home);
